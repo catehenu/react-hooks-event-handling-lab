2b03b9dc1d9c9f985544e42b9124870d4e610f53
cate henu <catherine.henu@student.moringaschool.com>
2024-11-05 14:39:52 +0300

    updated files

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,28 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+import React from "react";
+
+function EyesOnMe() {
+  function handleFocus() {
+    console.log("Good!")
+    console.log("Good!");
+  }
+
+  function handleBlur() {
+    console.log("Hey! Eyes on me!")
+    console.log("Hey! Eyes on me!");
+  }
+
+  return (
+    <button 
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+    >Click me</button>
+  )
+    <button onFocus={handleFocus} onBlur={handleBlur}>
+      Eyes on me
+    </button>
+  );
+}
+
+
+export default EyesOnMe;
